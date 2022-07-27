315adfaea17a60c010fad8b7643aca6d8eca3fa4
nautilus@porpoise <nautilus@trovaprezzi.it>
2022-07-27 15:04:28 +0200

    Fixed tests after changes in search action

--- spec/schema_dot_org/search_action_spec.rb
+++ spec/schema_dot_org/search_action_spec.rb
@@ -3,7 +3,6 @@
 require 'spec_helper'
 require 'schema_dot_org/search_action'
 
-
 RSpec.describe SchemaDotOrg::SearchAction do # rubocop:disable Metrics/BlockLength
   describe "#new" do
     it 'will not create a SearchAction without attributes' do
@@ -13,26 +12,27 @@ RSpec.describe SchemaDotOrg::SearchAction do # rubocop:disable Metrics/BlockLeng
     it 'creates a SearchAction when given a target and query input' do
       expect do
         SchemaDotOrg::SearchAction.new(
-          target:      'https://www.oregonlaws.org/?search={search_term_string}',
-          query_input: 'required name=search_term_string'
+          target: SchemaDotOrg::EntryPoint.new(urlTemplate: 'https://www.oregonlaws.org/?search={search_term_string}').to_json_struct,
+          query_input: SchemaDotOrg::PropertyValueSpecification.new(valueName: 'required name=search_term_string').to_json_struct
         )
       end.to_not raise_error
     end
   end
 
-
   describe "#to_json_struct" do
     it "has exactly the correct attributes and values" do
       action = SchemaDotOrg::SearchAction.new(
-        target: 'https://www.oregonlaws.org/?search={search_term_string}',
-        query_input: 'required name=search_term_string'
+        target: SchemaDotOrg::EntryPoint.new(urlTemplate: 'https://www.oregonlaws.org/?search={search_term_string}').to_json_struct,
+        query_input: SchemaDotOrg::PropertyValueSpecification.new(valueName: 'required name=search_term_string').to_json_struct
       )
-
       expect(action.to_json_struct).to eq(
-        'target' =>      'https://www.oregonlaws.org/?search={search_term_string}',
-        'query_input' => 'required name=search_term_string',
-        '@type' =>   'SearchAction'
-      )
+                                         '@type' => 'SearchAction',
+                                         'target' => { '@type' => "EntryPoint",
+                                                       'urlTemplate' => 'https://www.oregonlaws.org/?search={search_term_string}' },
+                                         'query-input' => { "@type" => "PropertyValueSpecification",
+                                                            "valueName" => 'required name=search_term_string',
+                                                            "valueRequired"=>"http://schema.org/True"}
+                                       )
     end
   end
 end
--- spec/schema_dot_org/web_site_spec.rb
+++ spec/schema_dot_org/web_site_spec.rb
@@ -6,46 +6,47 @@ require 'spec_helper'
 require 'schema_dot_org/web_site'
 require 'schema_dot_org/search_action'
 
-WebSite      = SchemaDotOrg::WebSite
+WebSite = SchemaDotOrg::WebSite
 SearchAction = SchemaDotOrg::SearchAction
-
 
 RSpec.describe WebSite do
   describe "#new" do
     it 'creates correct json without optional attributes' do
       basic_site = WebSite.new(
         name: 'Texas Public Law',
-        url:  'https://texas.public.law'
+        url: 'https://texas.public.law'
       )
 
       expect(basic_site.to_json_struct).to eq(
-        '@type' => 'WebSite',
-        'name' => 'Texas Public Law',
-        'url' => 'https://texas.public.law'
-      )
+                                             '@type' => 'WebSite',
+                                             'name' => 'Texas Public Law',
+                                             'url' => 'https://texas.public.law'
+                                           )
     end
-
 
     it 'creates correct json with the optional attributes' do
       site_with_search = WebSite.new(
         name: 'Texas Public Law',
-        url:  'https://texas.public.law',
+        url: 'https://texas.public.law',
         potential_action: SearchAction.new(
-          target: 'https://texas.public.law/?search={search_term_string}',
-          query_input: 'required name=search_term_string'
+          target: SchemaDotOrg::EntryPoint.new(urlTemplate: 'https://texas.public.law/?search={search_term_string}').to_json_struct,
+          query_input: SchemaDotOrg::PropertyValueSpecification.new(valueName: 'required name=search_term_string').to_json_struct
         )
       )
 
       expect(site_with_search.to_json_struct).to eq(
-        '@type' => 'WebSite',
-        'name' => 'Texas Public Law',
-        'url' => 'https://texas.public.law',
-        'potentialAction' => {
-          '@type' => 'SearchAction',
-          'target' => 'https://texas.public.law/?search={search_term_string}',
-          'query_input' => 'required name=search_term_string'
-        }
-      )
+                                                   '@type' => 'WebSite',
+                                                   'name' => 'Texas Public Law',
+                                                   'url' => 'https://texas.public.law',
+                                                   'potentialAction' => {
+                                                     '@type' => 'SearchAction',
+                                                     'target' => { '@type' => "EntryPoint",
+                                                                   'urlTemplate' => 'https://texas.public.law/?search={search_term_string}' },
+                                                     'query-input' => { "@type" => "PropertyValueSpecification",
+                                                                        "valueName" => 'required name=search_term_string',
+                                                                        "valueRequired" => "http://schema.org/True" }
+                                                   }
+                                                 )
     end
   end
 end
