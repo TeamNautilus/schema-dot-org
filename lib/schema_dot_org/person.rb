3b63821c7d5becbdcc71de02ec29cae758b692ac
nautilus@narwhal <nautilus@trovaprezzi.it>
2022-09-05 12:54:35 +0200

    author is a Thing (could be Organization or Person)

--- lib/schema_dot_org/person.rb
+++ lib/schema_dot_org/person.rb
@@ -5,17 +5,6 @@ require 'schema_dot_org'
 
 module SchemaDotOrg
   # Model the Schema.org `Person`.  See http://schema.org/Person
-  class Person < SchemaType
-    attr_accessor :name, :url
-    validates :name, type: String, presence: true
-    validates :url, type: String, allow_nil: true
-
-
-    def _to_json_struct
-      {
-        'url' => url,
-        'name' => self.name
-      }
-    end
+  class Person < Thing
   end
 end
--- schema_dot_org.gemspec
+++ schema_dot_org.gemspec
@@ -4,7 +4,7 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
 Gem::Specification.new do |spec|
   spec.name          = "schema_dot_org"
-  spec.version       = '1.6.6'
+  spec.version       = '1.6.7'
   spec.authors       = ["Robb Shecter"]
   spec.email         = ["robb@public.law"]
 
